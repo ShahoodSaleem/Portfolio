37ae76043c5d59a85a0bf4770dfea6848bd833f6
Make_money_with_shahood_saleem <shahoodsaleem123@gmail.com>
2024-09-17 16:16:00 +0500

    add chatbot for quick questions

---
+++ js/bot.js
@@ -0,0 +1,7 @@
+<>
+  <script>
+    window.embeddedChatbotConfig = {chatbotId}: "_cm54WaGI4EKS6bQs8Xiw", domain:
+    "www.chatbase.co" &rbrace;;
+  </script>
+  <script src="https://www.chatbase.co/embed.min.js" defer></script>
+</>;
